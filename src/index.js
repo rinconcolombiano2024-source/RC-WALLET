16dfc9cc1deaaf74ca562d814f6d08cf52e91f1b
rinconcolombiano2024-source <rinconcolombiano2024@gmail.com>
2026-05-25 22:14:55 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import App from "./App";
 
-import { MiniKitProvider }
-from "@worldcoin/minikit-js/minikit-provider";
-
-const root =
-  ReactDOM.createRoot(
-    document.getElementById("root")
-  );
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-
-  <MiniKitProvider>
-
+  <React.StrictMode>
     <App />
-
-  </MiniKitProvider>
-
+  </React.StrictMode>
 );
