88944c72bdcf984c48494a0bda93a104408ad6a4
rinconcolombiano2024-source <rinconcolombiano2024@gmail.com>
2026-05-25 08:47:13 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,34 @@
+import "../styles.css";
+
 export default function App() {
-  return <h1>RC Wallet</h1>;
+  return (
+    <div className="app">
+      <div className="card">
+        <h1>RC Wallet</h1>
+        <p>Recuperación de fondos Worldcoin</p>
+
+        <button>
+          Conectar World App
+        </button>
+
+        <hr />
+
+        <h2>Dirección</h2>
+
+        <h2>ETH Balance</h2>
+        <p>0</p>
+
+        <hr />
+
+        <h2>Enviar Tokens</h2>
+
+        <input placeholder="Dirección destino" />
+        <input placeholder="Cantidad" />
+
+        <hr />
+
+        <h2>Tokens Detectados</h2>
+      </div>
+    </div>
+  );
 }
