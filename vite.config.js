1b673c18f0aaba0af6ae75396cad3dde8f8003c3
rinconcolombiano2024-source <rinconcolombiano2024@gmail.com>
2026-05-29 06:07:30 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,17 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
+  optimizeDeps: {
+    include: [
+      "viem",
+      "@worldcoin/minikit-js"
+    ]
+  },
+
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
+  }
 });
