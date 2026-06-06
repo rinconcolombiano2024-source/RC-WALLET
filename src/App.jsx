b491f152d7ed853d2b64937b42567b3eb356976c
rinconcolombiano2024-source <rinconcolombiano2024@gmail.com>
2026-06-06 11:32:08 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -684,11 +684,11 @@ if (tokenInfo.isNative) {
   return;
 
 }
-
+else{
       // =========================
       // ERC20
       // =========================
-else{
+
       const iface =
   new ethers.Interface(
     ERC20_ABI
