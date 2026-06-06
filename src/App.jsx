406eff5a1013f7deb573b4a24bd79ff4ed460bd5
rinconcolombiano2024-source <rinconcolombiano2024@gmail.com>
2026-06-06 19:21:08 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1465,6 +1465,27 @@ export default function App() {
     };
 
   }, []);
+      useEffect(() => {
+
+  if (!status) return;
+
+  const timer =
+    setTimeout(() => {
+
+      if (
+        status !==
+        "Inicializando RC Wallet..."
+      ) {
+
+        setStatus("");
+      }
+
+    }, 3500);
+
+  return () =>
+    clearTimeout(timer);
+
+}, [status]);
 
   // =========================
   // UI
