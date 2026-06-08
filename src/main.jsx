c191105d1533537d9e2adac5106e1dbbc1282001
rinconcolombiano2024-source <rinconcolombiano2024@gmail.com>
2026-06-09 01:11:33 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
 
+import { MiniKit } from "@worldcoin/minikit-js";
+
+MiniKit.install();
+
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
