03bbfdbf3a29d05446c75e57d368fbaebe077983
rinconcolombiano2024-source <rinconcolombiano2024@gmail.com>
2026-06-26 00:26:18 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5174,
-  },
-  build: {
-    chunkSizeWarningLimit: 1200,
+    port: 5173,
   },
 });
